45a8a11d7ef7207837145eeb90afa1285bfd56ac
Agamemnon94 <94798272+agamemnon94@users.noreply.github.com>
2022-02-09 11:38:14 +0100

    First go

---
+++ app.js
@@ -0,0 +1,115 @@
+let myheaderdiv = document.getElementById('headernav');
+let myCardsOdd = document.querySelector('.cardodd');
+let myCardsEven = document.querySelector('.cardeven');
+let myH2 = document.querySelector('.h2_section1');
+let videoheader = document.querySelector('.videohead');
+let myHome = document.getElementById('home');
+let myContact = document.getElementById('contact');
+let myNews = document.getElementById('news');
+let myArrow = document.getElementById('scroll-to-top')
+let smoothScroll = document.querySelector('.scroll-to-top');
+let myCursorOval = document.querySelector('.rightCursor')
+let goContact = document.querySelector('.btnspan2');
+let paragrapheHeader = document.querySelector('.visibleP');
+let bigbody = document.getElementById('body');
+const menu = document.querySelector('.headernav');
+const btnMenu = document.querySelector('.btn__toggle__container');
+const aMenu1 = document.querySelector('.header')
+
+
+// Fonction qui gère l'apparition des différents éléments en fonction de la hauteur de scroll sur le body
+window.addEventListener("scroll", () => {
+    console.log(scrollY);
+    if (window.scrollY > 50) {
+        myHome.classList.add('funnyPaddin')
+        myContact.classList.add('funnyPaddin');
+        myNews.classList.add('funnyPaddin');
+    } if (window.scrollY > 150) {
+        myheaderdiv.classList.remove('navbg');
+    } if (window.scrollY > 500) {
+        myHome.classList.remove('funnyPaddin');
+        myContact.classList.remove('funnyPaddin');
+        myNews.classList.remove('funnyPaddin');
+    } if (window.scrollY > 500) {
+    } if (window.scrollY > 799) {
+        bigbody.classList.add('pink__body')
+        myH2.classList.add('h2apears');
+    } if (window.scrollY > 890) {
+        myheaderdiv.classList.add('headerfixed');
+        videoheader.classList.add('videoFixed');
+        myHome.classList.add('header-a-gold');
+        myContact.classList.add('header-a-gold');
+        myNews.classList.add('header-a-gold');
+        myArrow.classList.add('arrow-visible');
+    } if (window.scrollY > 1499) {
+        bigbody.classList.remove('pink__body');
+    } if (window.scrollY < 150) {
+        myheaderdiv.classList.add('navbg');
+    } if (window.scrollY === 0) {
+        myHome.classList.remove('funnyPaddin');
+        myContact.classList.remove('funnyPaddin');
+        myNews.classList.remove('funnyPaddin');
+    } if (window.scrollY < 799) {
+        bigbody.classList.remove('pink__body');
+    } if (window.scrollY < 860) {
+        myheaderdiv.classList.remove('headerfixed');
+        videoheader.classList.remove('videoFixed');
+        myHome.classList.remove('header-a-gold');
+        myContact.classList.remove('header-a-gold');
+        myNews.classList.remove('header-a-gold');
+        myArrow.classList.remove('arrow-visible');
+    }
+});
+// Fonction qui adouci le retour en haut de page
+smoothScroll.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+    // ↓ foncton pour scroller par...
+    // window.scrollBy(0, window.innerHeight);
+});
+
+// Pour le bouton toggle du header de la page Home
+// change la class de la div et le contenu texte de la balise "<p>"
+myCursorOval.addEventListener('click', () => {
+    myCursorOval.classList.toggle('leftCursor');
+    console.log('hello');
+    if (paragrapheHeader.innerText == "Click me") {
+        paragrapheHeader.innerText = "And now,";
+    } else {
+        paragrapheHeader.innerText = "Click me";
+    }
+
+
+});
+
+goContact.addEventListener('click', () => {
+    // location.href = "#end";
+    window.scrollTo({
+        top: 2500,
+        left: 0,
+        behavior: "smooth"
+    });
+});
+
+
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.toggle('active__menu');
+    myHome.classList.toggle('active__a');
+    myContact.classList.toggle('active__a');
+    myNews.classList.toggle('active__a');
+    console.log('Hello');
+
+})
+const btn1 = document.querySelector('.btn1');
+
+
+function burger() {
+    btn1.classList.toggle('active')
+}
+
+
+btn1.addEventListener('click', burger);
---
+++ slider.js
@@ -0,0 +1,176 @@
+let sliders = document.querySelectorAll('.slider');
+let liste = document.getElementById('ul');
+let myContactH3 = document.querySelector('.h3__whoami__container');
+let myVideoFullScreen = document.querySelector('.video__who__container');
+let smoothScroll = document.querySelector('.scroll-to-top');
+let myArrow = document.getElementById('scroll-to-top');
+let span1 = document.querySelector('.span1');
+let span2 = document.querySelector('.span2');
+let span3 = document.querySelector('.span3');
+let span4 = document.querySelector('.span4');
+let span5 = document.querySelector('.span5');
+let span6 = document.querySelector('.span6');
+let span7 = document.querySelector('.span7');
+let span8 = document.querySelector('.span8');
+let span9 = document.querySelector('.span9');
+let span10 = document.querySelector('.span10');
+let span11 = document.querySelector('.span11');
+let span12 = document.querySelector('.span12');
+let span13 = document.querySelector('.span13');
+let span14 = document.querySelector('.span14');
+let span15 = document.querySelector('.span15');
+let span16 = document.querySelector('.span16');
+let span17 = document.querySelector('.span17');
+
+
+// Fonction qui permet de créer un événement au click sur chaque élément d'une liste en partant du parent soit, "l'ul" vers chaque "li" comme pour les enfants de la "li".
+liste.addEventListener('click', function (e) {
+    let target = e.target; // Clicked element
+    while (target && target.parentNode !== ul) {
+        target = target.parentNode; // If the clicked element isn't a direct child
+        if (!target) { return; } // If element doesn't exist
+    }
+    if (target.tagName === 'LI') {
+        // console.log(target.id); // Check if the element is a LI
+        document.getElementById('h1__who__container').scrollIntoView({ block: "start", behavior: "smooth", top: 0, left: 0 });
+        // if (target.id === 'htmlwork') {
+        // } else { console.log('Ne fonctionne pas'); }
+    }
+});
+// Même fonction qui ci-dessus mais sans agire avec les enfants des "li", des <a> par exemple. dans ce projet il n'y en a pas.
+// liste.addEventListener('click', function (e) {
+//     if (e.target.tagName === 'LI') {
+//         console.log(e.target.id);  // Check if the element is a LI
+//     }
+// });
+
+sliders.forEach(function (slider) {
+    let sliderRange = slider.querySelector('.slider__range');
+    let sliderBefore = slider.querySelector('.slider__before');
+    let sliderSeparator = slider.querySelector('.slider__separator');
+
+    function updateSliderPosition() {
+        sliderBefore.style = `width:${sliderRange.value}%`;
+        sliderSeparator.style = `left:${sliderRange.value}%`;
+    }
+
+    sliderRange.addEventListener('input', updateSliderPosition);
+
+    let isDragging = false;
+    // console.log('false');
+
+    sliderSeparator.addEventListener('mousedown', function () {
+        isDragging = true;
+        // console.log('appuyée');
+    })
+    document.addEventListener('mouseup', function () {
+        isDragging = false
+        // console.log('relâchée');
+    })
+    document.addEventListener('mousemove', function (e) {
+        if (isDragging) {
+            let sliderP1 = slider.querySelector('.slider__p1');
+            let sliderP2 = slider.querySelector('.slider__p2');
+            let listWho = document.querySelector('.comp');
+            let sliderContainer = document.querySelector('.slider__container')
+            let sliderRect = slider.getBoundingClientRect();
+            let newWidth = (e.clientX - sliderRect.left) / sliderRect.width * 100;
+            sliderRange.value = newWidth;
+            updateSliderPosition();
+            // console.log(newWidth);
+
+            if (newWidth < 55) {
+                sliderP1.classList.add('slider__p__opacity');
+                sliderP2.style.opacity = '0';
+                sliderP2.style.transform = ('scale(0)');
+
+
+            } if (newWidth > 45) {
+                sliderP2.style.opacity = '1';
+                sliderP2.style.transform = ('scale(1)');
+                sliderP1.classList.remove('slider__p__opacity');
+
+            } if (newWidth < 0) {
+                // isDragging = false;
+                sliderContainer.classList.add('slider__container__left')
+                // sliderP1.classList.remove('slider__p__opacity');
+                listWho.classList.add('display__content');
+            } if (newWidth >= 100) {
+                sliderContainer.classList.remove('slider__container__left')
+                listWho.classList.remove('display__content');
+            }
+            // console.log(e);
+        }
+    })
+});
+
+myContactH3.addEventListener('click', function () {
+    myVideoFullScreen.requestFullscreen();
+    // if (myVideoFullScreen.requestFullscreen()) {
+    //     myContactH3.style.opacity = '0';
+    // }
+    // essayer de faire toggle avec .une__class.class__active
+})
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        // myContactH3.style.opacity = '1';
+    }
+})
+window.addEventListener("scroll", (e) => {
+    console.log(scrollY);
+    if (window.scrollY > 999) {
+        myArrow.classList.add('arrow-visible');
+        span1.classList.add('active__falls__later')
+        span2.classList.add('active__falls');
+        span3.classList.add('active__falls');
+        span4.classList.add('active__falls');
+        span5.classList.add('active__falls');
+        span6.classList.add('active__falls');
+        span7.classList.add('active__falls');
+        span8.classList.add('active__falls');
+        span9.classList.add('active__falls');
+        span10.classList.add('active__falls');
+        span11.classList.add('active__falls');
+        span12.classList.add('active__falls');
+        span13.classList.add('active__falls');
+        span14.classList.add('active__falls');
+        span15.classList.add('active__falls');
+        span16.classList.add('active__falls');
+        span17.classList.add('active__falls');
+    }
+    if (window.scrollY < 901) {
+        myArrow.classList.remove('arrow-visible');
+        span1.classList.remove('active__falls__later');
+        span2.classList.remove('active__falls');
+        span3.classList.remove('active__falls');
+        span4.classList.remove('active__falls');
+        span5.classList.remove('active__falls');
+        span6.classList.remove('active__falls');
+        span7.classList.remove('active__falls');
+        span8.classList.remove('active__falls');
+        span9.classList.remove('active__falls');
+        span10.classList.remove('active__falls');
+        span11.classList.remove('active__falls');
+        span12.classList.remove('active__falls');
+        span13.classList.remove('active__falls');
+        span14.classList.remove('active__falls');
+        span15.classList.remove('active__falls');
+        span16.classList.remove('active__falls');
+        span17.classList.remove('active__falls');
+    }
+
+});
+
+
+smoothScroll.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+    // ↓ foncton pour scroller par...
+    // window.scrollBy(0, window.innerHeight);
+});
+
+
+
